01c2e5ff41a0bc1dc09ca684af0374a2bf34e454
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-11-26 10:36:31 +0100

    Creada la clase Contacto así como  sus atributos correspondientes

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,10 @@
+package org.iesalandalus.programacion.agenda;
+
+public class Contacto {
+	private String ERR_TELEFONO;
+	private String ERR_CORREO;
+	private String nombre;
+	private String telefono;
+	private String correo;
+
+}
