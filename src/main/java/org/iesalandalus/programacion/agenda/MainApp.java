a04be4378d2b4d6eddb2438fb0b237a232e8eec7
Rafael <rafaelaybar@hotmail.com>
2018-12-08 04:32:31 +0100

    Terminado el método elegirOpcion

--- src/main/java/org/iesalandalus/programacion/agenda/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/agenda/MainApp.java
@@ -3,11 +3,19 @@ package org.iesalandalus.programacion.agenda;
 public class MainApp {
 	private String ERROR = "ERROR";
 	private String EXITO = "Operación realizada satisfactoriamente";
+	private int numeroPrincipal;
 
 	public void mostrarMenu() {
 		System.out.println("Selecciona qué quieres hacer");
 		System.out.println("1 añadir un contacto,  2 buscar un contacto, 3 borrar un contacto");
 		System.out.println("4 listar todos los contactos no nulos, 6 salir");
+	}
+
+	public int elegirOpcion(int numeroPrincipal) {
+		if (numeroPrincipal < 1 || numeroPrincipal > 6) {
+			System.out.println("La opción introducida es incorrecta");
+		}
+		return numeroPrincipal;
 	}
 
 	public static void main(String[] args) {
